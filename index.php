8dbb88a39ee76e2e7eac178f93c987b6d384d94d
Dari Popov <81808651+obama-max@users.noreply.github.com>
2023-01-12 22:16:56 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$message = "Tova raboti li?";
+echo "<h1> $message <h1/>";
+$message = "Okay raboti qko tuka trebva da vlezni database";
+echo "<h1> $message <h1/>";
+$message = "Sus css she vkarame danite i sled tova she gi nakarame da rabotqt s crude";
+echo "<h1> $message <h1/>";
+?>
