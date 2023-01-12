f3a9830a8c0c775281b65667e177da02874f62d6
Dari Popov <81808651+obama-max@users.noreply.github.com>
2023-01-13 01:45:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database</title>
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <?php
+
 $message = "Tova raboti li?";
 echo "<h1> $message <h1/>";
+
 $message = "Okay raboti qko tuka trebva da vlezni database";
-echo "<h1> $message <h1/>";
+echo "<h2> $message <h2/>";
+
+$id = "id";
+$firstname = "firstname";
+$lastname = "lastname";
+$phonenumber = "phonenumber";
+$email = "email";
+$adult = "adult";
+$capacity = "capacity";
+$destination = "destination";
+$transport = "transport";
+$stars = "stars";
+$Roomtype = "Room type";
+$vacany = "vacancy";
+$priceofadultbed = "priceadultbed";
+$priceofchildbed = "priceofchildbed";
+$number = "number";
+$nameofreservation ="nameofreservation";
+$typeoftransport = "typeoftransport";
+$user ="user";
+$numberofclientsgoingthere ="numberofclientsgoingthere";
+$dateofdeparture ="dateofdeparture";
+$dateofarrival = "dateofarrival";
+$includedbreakfast = "includedbreakfast";
+$allinclusive = "allinclusive";
+$payday = "payday";
+$username = "username";
+$password = "password";
+$surname = "surname";
+$IDN = "IDN";
+$dateofassigment = "dateofassigment";
+$activity = "activity";
+$dateofremoval = "dateofremoval";
+
 $message = "Sus css she vkarame danite i sled tova she gi nakarame da rabotqt s crude";
-echo "<h1> $message <h1/>";
+echo "<h3> $message <h3/>";
 ?>
+</body>
+</html>
